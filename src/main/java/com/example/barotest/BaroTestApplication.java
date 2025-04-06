d895f62f16428a0b69ff1f4c3563e9725d13cf40
HyangKeun Choi <gidrms99@naver.com>
2025-04-06 23:20:17 +0900

    first commit

---
+++ src/main/java/com/example/barotest/BaroTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.barotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaroTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaroTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/barotest/BaroTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.barotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaroTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
